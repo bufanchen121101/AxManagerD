594c8b37359f0ed5926fc50aa6a76aa527c79b25
小陈 <xx1112z@qq.com>
2026-08-18 20:33:46 +0800

    Initial commit

---
+++ manager/src/main/java/frb/axeron/manager/AxeronApplication.kt
@@ -0,0 +1,75 @@
+package frb.axeron.manager
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import coil.Coil
+import coil.ImageLoader
+import com.topjohnwu.superuser.Shell
+import frb.axeron.Axerish
+import frb.axeron.api.core.AxeronSettings
+import frb.axeron.api.core.Engine
+import frb.axeron.manager.ui.util.createShellBuilder
+import me.zhanghai.android.appiconloader.coil.AppIconFetcher
+import me.zhanghai.android.appiconloader.coil.AppIconKeyer
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import org.lsposed.hiddenapibypass.HiddenApiBypass
+import java.io.File
+import java.util.Locale
+
+class AxeronApplication : Engine() {
+    companion object {
+        lateinit var axeronApp: AxeronApplication
+
+        init {
+//            logd("ShizukuApplication", "init")
+            Axerish.initialize(BuildConfig.APPLICATION_ID)
+            Shell.setDefaultBuilder(createShellBuilder())
+            Shell.enableLegacyStderrRedirection = true
+            Shell.enableVerboseLogging = BuildConfig.DEBUG
+
+            if (Build.VERSION.SDK_INT >= 28) {
+                HiddenApiBypass.setHiddenApiExemptions("")
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                System.loadLibrary("adb")
+            }
+        }
+    }
+
+    lateinit var okhttpClient: OkHttpClient
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        axeronApp = this
+        AxeronSettings.initialize(axeronApp)
+    }
+
+    @SuppressLint("ResourceType")
+    override fun onCreate() {
+        super.onCreate()
+
+        val context = this
+        val iconSize = resources.getDimensionPixelSize(android.R.dimen.app_icon_size)
+        Coil.setImageLoader(
+            ImageLoader.Builder(context)
+                .components {
+                    add(AppIconKeyer())
+                    add(AppIconFetcher.Factory(iconSize, false, context))
+                }
+                .build()
+        )
+
+
+        okhttpClient =
+            OkHttpClient.Builder().cache(Cache(File(cacheDir, "okhttp"), 10 * 1024 * 1024))
+                .addInterceptor { block ->
+                    block.proceed(
+                        block.request().newBuilder()
+                            .header("User-Agent", "AxManager/${BuildConfig.VERSION_CODE}")
+                            .header("Accept-Language", Locale.getDefault().toLanguageTag()).build()
+                    )
+                }.build()
+    }
+}
